b820875d6a38be192fbb0c6b7db910d8cd5b7fd2
rifat mondol <rifatmondol791@gmail.com>
2021-03-24 19:18:03 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll",function(){
+    var header = document.querySelector(".header-section");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+// Scroll To Top 
+var scrollTop = $(".scrollToTop");
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() < 500) {
+        scrollTop.removeClass("active");
+    } 
+    else {
+        scrollTop.addClass("active");
+    }
+});
+//Click event to scroll to top
+$('.scrollToTop').on('click', function () {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 300);
+    return false;
+});
